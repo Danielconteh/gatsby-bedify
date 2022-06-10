1210fd483516e2249c7b668078f60d590720f6ce
Daniel conteh <contehdaniel1995@gmail.com>
2022-06-10 23:05:11 +0000

    nprogress

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,5 +57,17 @@ module.exports = {
         },
       },
     },
+
+
+    // Nprogress
+    {
+    resolve: `gatsby-plugin-nprogress`,
+    options: {
+      // Setting a color is optional.
+      color: `tomato`,
+      // Disable the loading spinner.
+      showSpinner: false,
+    },
+  },
   ],
 }
